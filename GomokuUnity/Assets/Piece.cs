043ac078c197b73b54fa40fe132baba68e739bdd
Yodi Pramudito <yodipramudito@yahoo.com>
2015-11-24 20:28:45 +0700

    fix color transparancy

--- GomokuUnity/Assets/Piece.cs
+++ GomokuUnity/Assets/Piece.cs
@@ -15,10 +15,10 @@ public class Piece : MonoBehaviour {
 	}
 
 	public void Blink(){
-		GetComponentInChildren<Renderer>().material.SetColor("_Color",new Color(4,4,4,1));
+		GetComponentInChildren<Renderer>().material.SetColor("_Color",new Color(4,4,4,0.7f));
 		DG.Tweening.DOTween.To(
 			()=>GetComponentInChildren<Renderer>().material.GetColor("_Color").r,
-			(float x)=>(GetComponentInChildren<Renderer>().material.SetColor("_Color",new Color(x,x,x,1))),
+			(float x)=>(GetComponentInChildren<Renderer>().material.SetColor("_Color",new Color(x,x,x,0.7f))),
 			1, 0.5f);
 	}
 }
